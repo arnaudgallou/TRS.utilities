62f6837c0e8d45835405b29afdda4aa2962f6722
arnaudgallou <arnaudgallou31@gmail.com>
2022-06-13 19:03:15 +0200

    replace `str_remove()` with `string_replace()`

--- R/helpers.R
+++ R/helpers.R
@@ -1,7 +1,7 @@
 get_mdl_settings <- function(x) {
   settings <- x$settings
   tibble(
-    model = str_remove(deparse(settings$formula), "^~"),
+    model = string_replace(deparse(settings$formula), "^~", ""),
     elevation_span = settings$min_elev_grad_len,
     exclusion_zone = settings$exclusion_zone,
     std_from = settings$std_from
