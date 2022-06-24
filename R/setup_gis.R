7b0150a46632bffb74ca6f6cef2b46d4318ef3cf
arnaudgallou <arnaudgallou31@gmail.com>
2022-06-24 08:26:51 +0200

    guard clauses

--- R/setup_gis.R
+++ R/setup_gis.R
@@ -2,10 +2,13 @@ setup_gis <- function () {
   proj <- "TRS.Rproj"
 
   if (!file.exists(proj)) {
-    stop("'setup_gis' must be executed in ", proj, ".")
+    stop('"setup_gis" must be executed in ', proj, ".")
+  }
+  if (!file.exists("data/trs.csv")) {
+    stop('"trs.csv" could not be found in "data".')
   }
 
-  x <- read_csv("main_df.csv", col_select = "location")
+  x <- read_csv("data/trs.csv", col_select = "location")
   mountains <- unique(x$location)
 
   dir_data <- c(
