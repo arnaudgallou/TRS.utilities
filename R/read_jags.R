42c7a8f50e0c3d84a0a0a6b39e1e96ba400b577f
arnaudgallou <arnaudgallou31@gmail.com>
2023-02-21 18:20:21 +0100

    rework `read_jags()`

--- R/read_jags.R
+++ R/read_jags.R
@@ -13,32 +13,22 @@ read_jags <- function(x, pluck, vars = NULL, ...) {
 #' @rdname read_jags
 #' @export
 read_jags.character <- function(x, pluck, vars = NULL, ...) {
-  ReadJAGS(x, pluck, vars)
-}
-
-#' @rdname read_jags
-#' @export
-read_jags.collection <- function(x, pluck, vars = NULL, ..., .id = NULL) {
-  map_df(x, ~ {
-    .x <- ReadJAGS(.x, pluck, vars)
-    tibble::rowid_to_column(.x)
-  }, .id = .id)
-}
-
-
-ReadJAGS <- function(x, pluck, vars) {
   if (!is.null(vars) && !(pluck %in% c("summary", "sims"))) {
-    stop("'vars' can only be used with 'summary' or 'sims'.")
+    abort("`vars` can only be used with `summary` or `sims`.", call = caller_env())
   }
-
   x <- read_rds(x)
   x <- x[[pluck]]
-
   if (!is.null(vars)) {
     y <- if (pluck == "summary") x$parameter else names(x)
     matched <- grepl(vars, y, perl = TRUE)
     x <- if (pluck == "summary") x[matched, ] else x[, matched]
   }
+  rowid_to_column(x)
+}
 
-  x
+#' @rdname read_jags
+#' @export
+read_jags.collection <- function(x, pluck, vars = NULL, ..., names_to = NULL) {
+  out <- map(x, \(file) read_jags(file, pluck, vars))
+  list_rbind(out, names_to = names_to)
 }
