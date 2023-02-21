1a2a03dbaf7099025c5601f4e62102f7a5fc1a1c
arnaudgallou <arnaudgallou31@gmail.com>
2023-02-21 21:38:04 +0100

    refactor

--- R/run_jags.R
+++ R/run_jags.R
@@ -8,111 +8,117 @@
 #' @param filename Name (without extension) of the file. Leave `filename=NULL` to use a default file name based on the model settings.
 #' @param default_output Should `run_jags()` return the model object returned by [`R2jags::jags()`]?
 #' @export
-run_jags <- function(x, model, ..., save = FALSE, path, filename = NULL, default_output = FALSE) {
-  if (isTRUE(save) && missing(path)) {
+run_jags <- function(
+    x,
+    model,
+    ...,
+    save = FALSE,
+    path,
+    filename = NULL,
+    default_output = FALSE
+) {
+  if (is_true(save) && missing(path)) {
     stop('"path" is missing, with no default.')
   }
-
   suppressWarnings(
-    out <- R2jags::jags(x$data, parameters.to.save = model$param, model.file = model$model, DIC = FALSE, ...)
+    out <- R2jags::jags(
+      x$data,
+      parameters.to.save = model$param,
+      model.file = model$model,
+      DIC = FALSE,
+      ...
+    )
   )
-
-  if (!isTRUE(default_output)) {
+  if (is_false(default_output)) {
     out <- make_jags_output(x, model$name, out)
   }
-
-  if (isTRUE(save)) {
-    save_jags(out, x$settings, path, filename)
-  } else {
-    out
+  if (is_false(save)) {
+    return(out)
   }
+  save_jags(out, x$settings, path, filename)
 }
 
-
 make_jags_output <- function(x, mdl_name, fit) {
-  vars <- x$settings$terms
-  clim_vars <- vars[!grepl("land_type", vars)]
-  expl_vars <- paste(vars, collapse = "_")
-
+  terms <- x$settings$terms
+  vars <- terms[!grepl("land_type", terms)]
+  expl_vars <- set_names(terms, paste0("expl_var", name_suffix(terms)))
   sims <- jags_array_to_tibble(fit$BUGSoutput$sims.array)
-
   tbl_data <- tbl_elev_grad_clim <- NULL
-
   if (mdl_name == "global_scale_mdl") {
-    settings_min <- tibble(
-      expl_var = expl_vars,
-      elevation_span = x$settings$elevation_span,
-      exclusion_zone = x$settings$exclusion_zone,
-      std_from = x$settings$std_from
-    )
-
+    tbl_data_settings <- data_settings(x, expl_vars, "global")
     tbl_data <- tibble(
-      settings_min,
+      tbl_data_settings,
       as_tibble(x$data[names(x$data) %in% x$settings$cols]),
       est_range_mean = fit$BUGSoutput$mean$alpha,
-      est_range_sd = fit$BUGSoutput$sd$alpha
+      est_range_sd = fit$BUGSoutput$sd$alpha,
+      se_min = .data$est_range_mean - .data$est_range_sd,
+      se_max = .data$est_range_mean + .data$est_range_sd
     )
-
-    if (length(clim_vars) == 1) {
-      tbl_data <- rename(tbl_data, bioclim = !!enquo(clim_vars))
+    if (length(vars) == 1L) {
+      tbl_data <- rename(tbl_data, x = !!enquo(vars))
     }
   } else {
-    settings_min <- tibble(
-      expl_var = expl_vars,
-      exclusion_zone = x$settings$exclusion_zone
-    )
+    tbl_data_settings <- data_settings(x, expl_vars, "local")
   }
-
   if (x$settings$average) {
     tbl_elev_grad_clim <- x$elev_grad_clim
   }
-
-  out <- list(
-    sims = tibble(settings_min, sims),
-    summary = tibble(
-      settings_min,
-      as_tibble(
-        fit$BUGSoutput$summary,
-        rownames = "parameter",
-        .name_repair = make_clean_names
-      )
-    ),
-    stats = list(
-      mean = fit$BUGSoutput$mean,
-      median = fit$BUGSoutput$median,
-      sd = fit$BUGSoutput$sd
-    ),
+  list(
+    sims = tibble(tbl_data_settings, sims),
+    summary = tibble(tbl_data_settings, as_tibble(
+      fit$BUGSoutput$summary,
+      rownames = "parameter",
+      .name_repair = make_clean_names
+    )),
+    stats = jags_statistics(fit),
     data = tbl_data,
     elev_grad_clim = tbl_elev_grad_clim,
-    settings = c(
-      x$settings,
-      list(
-        n_chains = fit$BUGSoutput$n.chains,
-        n_iter = fit$BUGSoutput$n.iter,
-        n_burnin = fit$BUGSoutput$n.burnin,
-        n_thin = fit$BUGSoutput$n.thin,
-        n_keep = fit$BUGSoutput$n.keep,
-        n_sims = fit$BUGSoutput$n.sims
-      )
-    ),
+    settings = c(x$settings, jags_settings(fit)),
     saved_parameters = fit$parameters.to.save,
     model = fit$model$model()
   )
-
-  out
 }
 
+data_settings <- function(x, expl_vars, scope) {
+  out <- tibble(
+    as_tibble_row(expl_vars),
+    exclusion_zone = x$settings$exclusion_zone
+  )
+  if (scope == "local") {
+    return(out)
+  }
+  tibble(
+    out,
+    elevation_span = x$settings$elevation_span,
+    std_from = x$settings$std_from
+  )
+}
+
+jags_statistics <- function(x) {
+  out <- x$BUGSoutput
+  list(mean = out$mean, median = out$median, sd = out$sd)
+}
+
+jags_settings <- function(x) {
+  out <- x$BUGSoutput
+  list(
+    n_chains = out$n.chains,
+    n_iter = out$n.iter,
+    n_burnin = out$n.burnin,
+    n_thin = out$n.thin,
+    n_keep = out$n.keep,
+    n_sims = out$n.sims
+  )
+}
 
 jags_array_to_tibble <- function(array) {
-  array %>%
-    as.data.frame.table(base = list(as.character(seq_along(array)))) %>%
-    as_tibble() %>%
-    rename(iter = .data$Var1, chain = .data$Var2) %>%
-    mutate(iter = as.integer(.data$iter)) %>%
-    pivot_wider(names_from = .data$Var3, values_from = .data$Freq) %>%
-    clean_names()
+  out <- as.data.frame.table(array, base = list(as.character(seq_along(array))))
+  out <- as_tibble(out)
+  out <- rename(out, iter = .data$Var1, chain = .data$Var2)
+  out <- mutate(out, iter = as.integer(.data$iter))
+  out <- pivot_wider(out, names_from = .data$Var3, values_from = .data$Freq)
+  clean_names(out)
 }
-
 
 make_filename <- function(x, filename) {
   if (!is.null(filename)) {
@@ -122,7 +128,6 @@ make_filename <- function(x, filename) {
   terms <- paste(x$terms, collapse = "_")
   glue("{terms}-span_{x$elevation_span}-excl_{x$exclusion_zone}{tail}.rds")
 }
-
 
 save_jags <- function(x, settings, path, filename) {
   if (!dir.exists(path)) {
