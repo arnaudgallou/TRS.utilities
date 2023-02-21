b7ce9004eaefe75ec8db3b12873ace9ca6885840
arnaudgallou <arnaudgallou31@gmail.com>
2023-02-21 18:29:28 +0100

    refactor `compose_mdl_data()`

--- R/compose_mdl_data.R
+++ R/compose_mdl_data.R
@@ -5,10 +5,10 @@
 #' @return A list of data to pass on to a JAGS model.
 #' @export
 compose_mdl_data <- function(data, matrix_formula = NULL) {
-  data[] <- lapply(data, function(x) if (is.character(x)) as.factor(x) else x)
+  data[] <- lapply(data, \(x) if (is.character(x)) as.factor(x) else x)
   data_ls <- as.list(data)
 
-  n_data_ls <- lapply(data_ls, function(x) if (is.factor(x)) length(unique(x)) else NA)
+  n_data_ls <- lapply(data_ls, \(x) if (is.factor(x)) length(unique(x)) else NA)
   n_data_ls <- setNames(n_data_ls, paste0("n_", names(n_data_ls)))
   n_data_ls <- Filter(Negate(anyNA), n_data_ls)
 
@@ -18,7 +18,10 @@ compose_mdl_data <- function(data, matrix_formula = NULL) {
 
   if (!is.null(matrix_formula)) {
     model_matrix <- model.matrix(matrix_formula, data)
-    model_matrix_ls <- list(model_matrix = model_matrix, n_param = ncol(model_matrix))
+    model_matrix_ls <- list(
+      model_matrix = model_matrix,
+      n_param = ncol(model_matrix)
+    )
   }
 
   c(data_ls, n_data_ls, n = n_data, model_matrix_ls)
