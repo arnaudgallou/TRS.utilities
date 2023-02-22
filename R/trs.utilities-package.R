1d5896f9793b69b620f7497af9c5898585d13d3c
arnaudgallou <arnaudgallou31@gmail.com>
2023-02-22 12:03:34 +0100

    update

--- R/trs.utilities-package.R
+++ R/trs.utilities-package.R
@@ -1,16 +1,33 @@
-#' @description Package containing helper functions to reproduce results from the TRS project.
+#' @description Helpers functions to reproduce results from the TRS project.
 #' @keywords internal
-#' @importFrom rlang .data is_formula is_empty quos quo enquo enexprs parse_exprs := %||%
-#' @importFrom dplyr %>% mutate summarize select across filter between rowwise group_by ungroup group_vars if_else pull distinct bind_cols arrange left_join rename slice_sample n vars tibble as_tibble all_of any_of starts_with ends_with matches rename_with desc relocate contains
-#' @importFrom ggplot2 ggplot aes geom_abline geom_ribbon geom_line geom_point geom_errorbar geom_vline geom_hline geom_segment facet_wrap facet_grid as_labeller scale_color_manual scale_fill_manual scale_shape_manual scale_y_continuous scale_colour_brewer theme element_blank element_text margin unit xlab coord_cartesian geom_histogram labs geom_smooth scale_alpha_manual label_parsed
-#' @importFrom tibble rowid_to_column
+#' @importFrom rlang .data := %||%
+#' @importFrom rlang quos quo enquo exprs parse_exprs fn_fmls
+#' @importFrom rlang is_formula is_empty is_false is_true
+#' @importFrom rlang abort caller_env
+#' @importFrom tidyselect all_of any_of starts_with ends_with matches contains
+#' @importFrom tidyselect vars_select_helpers
+#' @importFrom dplyr mutate summarize across select pull distinct arrange relocate
+#' @importFrom dplyr filter between rowwise slice_sample
+#' @importFrom dplyr group_by ungroup group_vars
+#' @importFrom dplyr bind_cols left_join
+#' @importFrom dplyr n vars desc if_else
+#' @importFrom dplyr rename rename_with
+#' @importFrom ggplot2 ggplot aes
+#' @importFrom ggplot2 geom_abline geom_line geom_vline geom_hline geom_segment
+#' @importFrom ggplot2 geom_point geom_errorbar geom_histogram
+#' @importFrom ggplot2 geom_ribbon geom_smooth
+#' @importFrom ggplot2 facet_wrap facet_grid as_labeller label_parsed
+#' @importFrom ggplot2 scale_color_manual scale_colour_brewer scale_fill_manual
+#' @importFrom ggplot2 scale_shape_manual scale_y_continuous scale_alpha_manual
+#' @importFrom ggplot2 theme element_blank element_text margin unit
+#' @importFrom ggplot2 coord_cartesian labs xlab
+#' @importFrom tibble tibble as_tibble rowid_to_column
 #' @importFrom tidyr pivot_wider nest unnest drop_na
 #' @importFrom readr write_rds read_rds read_csv
-#' @importFrom purrr map_df map map2 imap_dfr pluck
+#' @importFrom purrr map map2 imap pluck list_rbind
 #' @importFrom stats setNames density median quantile var model.matrix
 #' @importFrom janitor clean_names make_clean_names
 #' @importFrom glue glue
-#' @importFrom toolkit has_metachr has_names sniff_dfs round_nearest proportion standardize parse_formula parse_num make_dir make_list delta string_replace string_replace_all string_extract string_extract_all string_strip string_clean uc_first first_word to_snake_case vec_to_chr_class list_files extract_file_name line_0 theme_elesic theme_blank_y assign_to_missing round_num
 #' @importFrom utils modifyList
 #' @importFrom colorspace darken
 "_PACKAGE"
