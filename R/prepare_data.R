381651001c1bd6c29b70a925262492ea33faf7f2
arnaudgallou <arnaudgallou31@gmail.com>
2023-02-22 10:10:40 +0100

    implement `make_posterior_data()`

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -34,3 +34,70 @@ calc_pred_conf <- function(files) {
   })
   list_rbind(out)
 }
+
+#' @title Prepare data for plotting posterior distribution
+#' @description Structure data for [`posterior_distribution()`].
+#' @param files Files to get data from.
+#' @param yvar Variable describing the vertical axis.
+#' @param prob The probability mass to include in the shaded region.
+#' @param prob_outer The probability mass to include in the outer interval.
+#' @param scales A scaling factor to scale the height of the ridgelines. See
+#'   [`ggridges::geom_ridgeline()`][] for details.
+#' @param labels A named vector of labels used for plotting. Names must be the
+#'   same as the explanatory variables.
+#' @param reverse Should the order of posterior distributions be reversed?
+#' @export
+make_posterior_data <- function(
+    files,
+    yvar,
+    prob = c(.8, .95),
+    prob_outer = 0.99,
+    scales = .01,
+    labels = NULL,
+    reverse = FALSE
+) {
+  if (length(scales) > 1L && any(!have_name(scales))) {
+    abort("`scales` must be a named vector when using more than one scale.")
+  }
+  if (!is.null(labels) && any(!have_name(labels))) {
+    abort("`labels` must be a named vector.")
+  }
+  out <- read_jags(files, "sims", vars = "span|zone|expl|beta_2")
+  out <- group_by(out, .data$expl_var, .data$elevation_span, .data$exclusion_zone)
+  out <- add_posterior_density(out, .data$beta_2, prob, outer_prob)
+  out <- ungroup(out)
+  out <- mutate(
+    out,
+    across(starts_with("y"), \(x) rescale(x, scales)),
+    !!yvar := relevel(yvar, reverse),
+  )
+  if (!is.null(labels)) {
+    out <- mutate(out, expl_var = relabel(labels))
+  }
+  out
+}
+
+rescale <- function(x, scales) {
+  if (length(scales) == 1L) {
+    return(x * scales)
+  }
+  data <- pick(expl_var)
+  case_when(!!!parse_exprs(
+    glue("data$expl_var == '{names(scales)}' ~ x * {scales}")
+  ))
+}
+
+relevel <- function(x, reverse) {
+  data <- pick(x)
+  levels <- unique(data$x)
+  if (reverse) {
+    levels <- rev(levels)
+  }
+  factor(data$x, levels = levels)
+}
+
+relabel <- function(labels) {
+  data <- pick(expl_var)
+  out <- str_replace_all(data$expl_var, labels)
+  factor(out, levels = labels)
+}
